b857e2802c36324f1e3cac4620c936c8eddc1ba8
paullsshin <paullsshin@hotmail.com>
2023-04-27 13:53:59 -0700

    trying to fix bug

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
 
   db.once("open", () => {
-    app.listen(process.env.PORT || 3001, () => {
+    app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`

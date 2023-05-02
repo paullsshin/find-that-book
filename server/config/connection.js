b8ea26d99d22ca82ef51a1253aebc26a4b7b9e96
paullsshin <paullsshin@hotmail.com>
2023-05-02 10:35:56 -0700

    trying to debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017",
+  process.env.MONGODB_URI || "mongodb+srv://paull:dqAfnLEpoF0hIU8J@find-that-book.rkb6fs8.mongodb.net/test",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
